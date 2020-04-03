4e757dd341e58655bea775a2e77da577671dced8
Nico Audy <nicoaudy1994@gmail.com>
2020-04-03 13:07:43 +0700

    create protected route and try the logged in

--- imports/startup/routes.js
+++ imports/startup/routes.js
@@ -1,17 +1,57 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import { Meteor } from "meteor/meteor";
+import { useTracker } from "meteor/react-meteor-data";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Redirect
+} from "react-router-dom";
 import { Login, Welcome, Counter } from "../ui/pages";
+
+const useAccount = () =>
+	useTracker(() => {
+		const user = Meteor.user();
+		const userId = Meteor.userId();
+		return {
+			user,
+			userId,
+			isLoggedIn: !!userId
+		};
+	}, []);
+
+function PrivateRoute({ children, ...rest }) {
+	const { isLoggedIn } = useAccount();
+	return (
+		<Route
+			{...rest}
+			render={({ location }) =>
+				isLoggedIn ? (
+					children
+				) : (
+					<Redirect
+						to={{
+							pathname: "/signin",
+							state: { from: location }
+						}}
+					/>
+				)
+			}
+		/>
+	);
+}
 
 export const Routes = () => {
 	return (
 		<Router>
 			<Switch>
-				<Route path="/login">
+				<Route path="/signin">
 					<Login />
 				</Route>
-				<Route path="/counter">
+				<PrivateRoute path="/counter">
 					<Counter />
-				</Route>
+				</PrivateRoute>
 				<Route path="/">
 					<Welcome />
 				</Route>
--- imports/ui/pages/Login.jsx
+++ imports/ui/pages/Login.jsx
@@ -1,3 +1,17 @@
 import React from "react";
 
-export default Login = () => <h1>Login</h1>;
+import { useHistory, useLocation } from "react-router-dom";
+
+export default Login = () => {
+	let history = useHistory();
+	let location = useLocation();
+
+	let { from } = location.state || { from: { pathname: "/" } };
+
+	return (
+		<div>
+			<p>You must log in to view the page at {from.pathname}</p>
+			<button onClick={() => {}}>Log in</button>
+		</div>
+	);
+};
